755ead22b2fef92bab27d28e972fb9eb22b7e655
Newton Ombese <newtonombese1@gmail.com>
2023-10-13 17:04:18 +0300

    Add test case for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -12,4 +12,19 @@ describe Solver do
       expect(Solver.new.reverse('hello')).to eq('olleh')
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" for 3' do
+      expect(Solver.new.fizzbuzz(3)).to eq('fizz')
+    end
+    it 'returns "buzz" for 5' do
+      expect(Solver.new.fizzbuzz(5)).to eq('buzz')
+    end
+    it 'returns "fizzbuzz" for 15' do
+      expect(Solver.new.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+    it 'returns the number as a string for any other number' do
+      expect(Solver.new.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
