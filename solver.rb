07aa5f8eb70b2a3212f1ec084fdd9e8c041c087f
GhostEsso <lilipitaham@gmail.com>
2023-10-13 15:59:03 +0000

    linters error corrected

--- solver.rb
+++ solver.rb
@@ -1,29 +1,28 @@
 class Solver
-    def factorial(num)
-      raise ArgumentError, "Factorial of negative number can't be calculated" if num.negative?
-  
-      num_factorial = 1
-      (1..num).each { |p| num_factorial *= p }
-      num_factorial
-    end
+  def factorial(num)
+    raise ArgumentError, "Factorial of negative number can't be calculated" if num.negative?
 
-    def reverse(string)
-        split_str = string.chars
-        reverse_str = []
-        split_str.each { |str| reverse_str.unshift(str) }
-        reverse_str.join
-    end
+    num_factorial = 1
+    (1..num).each { |p| num_factorial *= p }
+    num_factorial
+  end
 
-    def fizzbuzz(num)
-        if (num % 3).zero? && (num % 5).zero?
-          'fizzbuzz'
-        elsif (num % 5).zero?
-          'buzz'
-        elsif (num % 3).zero?
-          'fizz'
-        else
-          num.to_s
-        end
-      end
-    end    
-    
+  def reverse(string)
+    split_str = string.chars
+    reverse_str = []
+    split_str.each { |str| reverse_str.unshift(str) }
+    reverse_str.join
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
+  end
+end
